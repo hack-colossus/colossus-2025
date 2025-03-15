a9ea239b188c15103b59f978b0801c11fb16dd18
Sujan <work.sujan26@gmail.com>
2025-03-15 22:39:52 +0530

    add favicon and better meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import "./globals.css";
 import { VT323 } from "next/font/google";
 import Header from "./components/Header";
@@ -10,8 +11,30 @@ const vt323 = VT323({ weight: "400", subsets: ["latin"] });
 export const metadata = {
   title: "Colossus 2.O",
   description: "Join us for an exciting retro-themed space hackathon event!",
+  keywords:
+    "hackathon, space, retro, coding, programming, tech event, coding challenge, events, hackathons, space hackathon, retro hackathon",
   icons: {
     icon: "/images/favicon.ico",
+  },
+  openGraph: {
+    title: "Colossus 2.O | Retro-Themed Space Hackathon",
+    description: "Join us for an exciting retro-themed space hackathon event!",
+    images: [
+      {
+        url: "/images/og-image.jpg", // Create and add a shareable image
+        width: 1200,
+        height: 630,
+        alt: "Colossus 2.O Hackathon",
+      },
+    ],
+    type: "website",
+    locale: "en_US",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Colossus 2.O | Retro-Themed Space Hackathon",
+    description: "Join us for an exciting retro-themed space hackathon event!",
+    images: ["/images/twitter-image.jpg"],
   },
 };
 
@@ -34,6 +57,20 @@ export default function RootLayout({
         <link
           href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
           rel="stylesheet"
+        />
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Event",
+              name: "Colossus 2.O Hackathon",
+              description:
+                "Join us for an exciting retro-themed space hackathon event!",
+              // Add more event details like date, location, etc.
+            }),
+          }}
         />
       </head>
       <body
